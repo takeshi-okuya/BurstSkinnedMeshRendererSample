eafb01cac6c7f5b723c81f81199a07cd3688884d
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2021-02-22 04:52:19 +0900

    Add Unity Files

---
+++ Assets/Scenes/FpsCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FpsCounter : MonoBehaviour
+{
+    Text text;
+    int preTime = 0;
+    int preFrame = 0;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        int currentTime = (int)Time.time;
+
+        if (preTime < currentTime)
+        {
+            int currentFrame = Time.frameCount;
+            int fps = currentFrame - preFrame;
+            text.text = $"{fps} fps\r\n{(1000f / fps).ToString("N1")} ms";
+
+            preTime= currentTime;
+            preFrame = currentFrame;
+        }
+    }
+}
